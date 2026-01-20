ced38f6cbbb33965d079c226c05815b0fb3a082d
Lev Soukhanov <->
2026-01-21 01:53:57 +0400

    new proofs new me

--- main.py
+++ main.py
@@ -20,146 +20,194 @@ R = 2188824287183927522224640574525727508854836440041603434369820418657580849561
 E = (Q ** 12 - 1) // R
 
 class Cfg:
-    ### lookup_size parameter is bitsize of the lookup
-    ### 1 means no lookup allowed
-    def __init__(self, lookup_size, batching_level):
-        self.lookup_size = lookup_size
-        self.batching_level = batching_level
+    def __init__(self):
         self.n_wtns = 0
-        self.n_rk = 0
+        self.n_gates = 0
 
-    ### cost of the rangecheck of an already allocated element
-    def pay_range_check(self, n):
-        # allocate new witness in a permutation
-        if self.lookup_size > 1:
-            self.n_rk += n
-            self.n_wtns += n
-        else:
-            # it is a single bit, so we just do normal all-accept ADP that has rank 1
-            self.n_rk += n
+    ### c = sqrt(ab) gate
+    def mul_sq(self):
+        self.n_gates += 1
+        self.n_wtns += 1
 
-    ### non-optimized multiplication
-    def pay_mul(self, n):
-        self.n_rk += 2 * n
-        self.n_wtns += n
+    # Extension finding gadget, outputs s_i basis of the extension
+    def extension_basis_finder(self):
+        self.n_gates += 22
+        self.n_wtns += 11
 
-    def pay_batch_mul(self, width, n):
-        if self.batching_level == 0:
-            self.pay_mul(width * n)
-        elif self.batching_level == 1:
-            self.n_rk += 2 * n
-            self.n_wtns += width * n
-        else:
-            assert False
+    # Checks that value is in the base field. Both bitcheck and frobcheck cost the same
+    def basefield_check(self):
+        self.n_wtns += 253
+        self.n_gates += 254
 
-    ### validate that value is in the base field
-    def pay_base_field_check(self, n):
-        n_limbs = div_ceil(254, self.lookup_size)
-        self.n_wtns += (n_limbs - 1) * n
-        self.pay_range_check(n_limbs * n)
+    # Gadget that takes G1 point (x, y) and outputs u_i = x s_i, v_i = y s_i
+    # Performs Frobcheck of x, y and also checks that (x, y) is on curve
+    def miller_bases(self):
+        self.basefield_check() # x
+        self.basefield_check() # y
+        self.n_gates += 1 # y * y = (x_3 + b) * 1
+        self.n_wtns += 2 * 22 # introduce all u_i, v_i for i > 1
+        self.n_gates += 2 * 44 # constrain them + certify them using the squaring trick
+    
+    # Outputs a twist by element of mu_{32}, which is a cofactor of the odd-order subgroup of (Fq^{12})^*
+    # ---
+    tmp = Q ** 12 - 1
+    assert(tmp % 32 == 0)
+    assert(tmp % 64 != 0)
+    # ---
+    def twist(self):
+        self.n_wtns += 5 # t, t2, t4, t8, t16 
+        self.n_gates += 5 # tt = t2, t2t2 = t4, t4t4 = t8, t8t8 = t16, t16t16 = 1
 
-    def pay_is_on_curve(self, n):
-        # x2 = x * x
-        # x3 = x * x2
-        # these two are batchable
-        # single constraint to check (x3 - B) = y * y 
-        self.pay_batch_mul(2, n)
-        self.pay_mul(n)
-        self.n_wtns -= n # as we do not actually pay for result, just constrain
+    def miller_iter(self, pseudo_bit):
+        self.twist()
+        self.mul_sq()
+        if pseudo_bit != 0:
+            self.twist()
+            self.mul_sq()
+        self.mul_sq()
+        self.mul_sq()
+    
+    def miller_loop(self):
+        self.miller_iter(1)
+        for v in pseudo_binary_encoding[63::-1]:
+            self.miller_iter(v)
 
+    def odd_exp_fixpow(self, N):
+        n_bits = 0
+        while N > 0:
+            N >>= 1
+            n_bits += 1
+        for i in range(n_bits):
+            self.mul_sq()
 
-    ### cost of the initialization
-    def pay_prelude(self):
-        # lookups cost (num_lookuped_element + table_size) rank
-        # and (2 * num_lookuped_element + table_size) witness
-        # so in the prelude we pay 1 table size in both witness and rank
-        if self.lookup_size > 1:
-            self.n_rk += 2 ** self.lookup_size
-            self.n_wtns += 2 ** self.lookup_size
-        # need to normalize inputs
-        self.pay_base_field_check(8) # 2 G1 points and 1 G2 point
-        # find element x satisfying defining equation of Fq^12
-        self.n_wtns += 12
-        self.n_rk += 24 # approx, actually it is maybe 28 when we use batching_level = 0?
-        self.pay_is_on_curve(3)
+    def odd_exp_varpow(self, bitwidth):
+        for i in range(bitwidth):
+            self.mul_sq()
+            self.mul_sq()
 
-    ### doubles the point and simultaneously computes linefunc in another point
-    ### f <- f^2 * linefunc(T, T, P)
-    ### T <- 2T
-    def pay_miller_double(self):
-        # slope is 3x^2 / 2y
-        # x_2 = x * x
-        # s * y = 3/2 x_2
-        # y_inv * y = 1
-        # last 2 are batched
-        self.pay_mul(1)
-        self.pay_batch_mul(2, 1) # 2 new witnesses introduced which is accidentally correct
-        # introduce new point
-        self.n_wtns += 2
-        self.pay_is_on_curve(1)
-        # evaluate (y_2T - y_T) - s * (x_2T - x_T)
-        # simultaneously check that they do not coincide (x_2T - x_T) * x_inv = 1
-        # once again 2 new witnesses introduced - eval and x_inv
-        self.pay_batch_mul(2, 1)
-        # f2 = f * linefunc
-        # f_new = f * f2
-        # can be batched, lol
-        self.pay_batch_mul(2, 1)
+    # This skips finding extension basis as we will do it in the main phase
+    def verify_pairing_eq(self):
+        self.miller_bases()
+        self.miller_bases()
+        self.miller_loop()
+        self.miller_loop()
+        self.miller_loop()
+        self.odd_exp_varpow(130*7)
+        self.mul_sq()
+        self.mul_sq()
+        self.mul_sq()
+        self.odd_exp_fixpow(E)        
+        self.odd_exp_varpow(254)
+        self.odd_exp_varpow(254)
+        self.odd_exp_varpow(254)
+        self.odd_exp_varpow(254)
+        self.odd_exp_varpow(254)
+        self.mul_sq()
+        self.mul_sq()
+        self.mul_sq()
+        self.mul_sq()
+        self.n_gates += 1
 
-    ### f <- f * linefunc(T, Q, P), T <- T + Q
-    def pay_miller_add(self):
-        # slope is (y2-y1)/(x2 - x1)
-        # simultaneously check that x2-x1 != 0,
-        # which also validates that points are different
-        self.pay_batch_mul(2, 1)
-        # introduce new point
-        self.n_wtns += 2
-        self.pay_is_on_curve(1)
-        # evaluate slope (y3 - y1) - s * (x3 - x1)
-        # simultaneously evaluate that x3 - x1 is nonzero
-        self.pay_batch_mul(2, 1)
-        # validate also that x2 - x1 is nonzero
-        self.pay_mul(1)
-        # multiply f by linefunc output
-        self.pay_mul(1)
+    # compute 5-th power
+    def sbox(self):
+        self.n_wtns += 3
+        self.n_gates += 5
 
-    def pay_miller_loop(self):
-        for val in pseudo_binary_encoding[63::-1]:
-            self.pay_miller_double()
-            if val != 0:
-                self.pay_miller_add()
-        # frobenius twists of Q
-        self.pay_base_field_check(4) # to parse two coordinates into G2 point
-        # after that, Frobenius is linear
-        # multiply 4 elements by 12 coeffs of 1st frobenius and 12 coeffs of 2nd one
-        self.pay_batch_mul(24, 4)
-        # actually this definitely can be double-batched using cross-products...
-        self.pay_miller_add()
-        self.pay_miller_add()
+    # width 4 permutation, certify *dropped* outputs
+    def poseidon(self):
+        width = 4
+        n_full_rounds = 8
+        n_part_rounds = 54 # approx, need to check the script for Fq specifically
+        for i in range(n_full_rounds * width + n_part_rounds):
+            self.sbox()
+        for i in range(width - 1):
+            self.basefield_check()
+    
+    def verify_arith(self):
+        self.n_gates += 1
+        self.n_wtns += 254
+        self.n_gates += 254 # z^2 fully fits
 
-    ### pay exponentiation in Fq12
-    ### method - compute all x^2^k, then multiply ones that
-    ### we need by using running product 
-    def pay_exp(self, e):
-        n_ones = 0
-        log_e = 0
-        while e > 0:
-            n_ones += e % 2 # count bits equal to 1
-            log_e += 1
-            e //= 2
-        self.pay_mul(log_e - n_ones)
-        self.pay_batch_mul(2, n_ones)
+        self.n_gates += 4
+        self.n_wtns += 130 * 5 # computing z^3 in a non-reduced form \sum a_i 2^{64 i}
+        self.n_gates += 130 * 5 # 5 limbs (because z is 2 limb and z^2 is 4 limb), each limb is 128 + < 10
+
+        self.n_gates += 4
+        self.n_wtns += 130 * 7
+        self.n_gates += 130 * 7 # computing z^4
+
+        # multiplying p_i' by phi^i
+        # p_0'
+        self.odd_exp_varpow(254)
+        # p_1'
+        self.odd_exp_varpow(254)
+        self.odd_exp_varpow(126)
+        # p_2'
+        self.odd_exp_varpow(254)
+        self.odd_exp_varpow(254)
+        # p_3'
+        self.odd_exp_varpow(254)
+        self.odd_exp_varpow(130 * 5)
+
+        for i in range(4):
+            self.odd_exp_varpow(254) # v_i psi^i
+            self.basefield_check() # check v_i
+        for i in range(6):
+            self.mul_sq()
+
+        # checking v1 v2 - v3 = z (v0 vZ + u + z)
+        
+        # v1v2 - v3
+        self.n_gates += 4
+        self.n_wtns += 130 * 7
+        self.n_gates += 130 * 7
+
+        # v0vZ + u + z
+        self.n_gates += 4
+        self.n_wtns += 130 * 7
+        self.n_gates += 130 * 7
+
+        # varpows
+        self.odd_exp_varpow(130 * 7)
+        self.odd_exp_varpow(130 * 7)
+        self.odd_exp_varpow(128)
+
+    def verify_full(self):
+        (w, g) = (self.n_wtns, self.n_gates)
+        self.extension_basis_finder()
+        print("Extension:")
+        print("wtns = {}, gates = {}".format(self.n_wtns - w, self.n_gates - g))
+        (w, g) = (self.n_wtns, self.n_gates)
+        self.basefield_check() #check u
+        for i in range(4):
+            self.basefield_check() # check p_i
+        print("Glue:")
+        print("wtns = {}, gates = {}".format(self.n_wtns - w, self.n_gates - g))
+        (w, g) = (self.n_wtns, self.n_gates)
+ 
+        self.poseidon() # z = Hash(pp, u, P.x, P.y)
+        print("Hash:")
+        print("wtns = {}, gates = {}".format(self.n_wtns - w, self.n_gates - g))
+        (w, g) = (self.n_wtns, self.n_gates)
+        
+        self.basefield_check() # check z
+        self.verify_arith()
+        print("Arithmetic:")
+        print("wtns = {}, gates = {}".format(self.n_wtns - w, self.n_gates - g))
+        (w, g) = (self.n_wtns, self.n_gates)
+ 
+        self.verify_pairing_eq() # P, Q checked by miller_bases routine
+        print("Pairing equation:")
+        print("wtns = {}, gates = {}".format(self.n_wtns - w, self.n_gates - g))
+        (w, g) = (self.n_wtns, self.n_gates)
+ 
 
     def ciphertext_n_bytes(self):
-        return ((self.n_rk + 1) ** 2 * self.n_wtns * 32)
+        return ((2 * self.n_gates + 1) ** 2 * self.n_wtns * 32)
 
 def div_ceil(a, b):
     return (a + b - 1) // b
 
-cfg = Cfg(lookup_size=1, batching_level=0)
-cfg.pay_prelude()
-cfg.pay_miller_loop()
-cfg.pay_miller_loop()
-cfg.pay_exp(E)
-
+cfg = Cfg()
+cfg.verify_full()
 print("{x} TB".format(x = cfg.ciphertext_n_bytes() // 2**40))
